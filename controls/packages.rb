8a5b2c2379a7dbb447b244c8cacb9475697ff720
Nathan Weddle <nweddle@chef.io>
2017-02-21 13:17:37 -0800

    Rename Compliance Profile

--- controls/packages.rb
+++ controls/packages.rb
@@ -2,6 +2,7 @@ control 'package-1' do
   impact 0.5
   title 'Ensure Slack Windows app is installed'
   desc '
+  Windows Slack application should be installed.
   '
   tag 'windows', 'application'
   tag cce: 'CCE-27072-8'
